e676de621070bc3eb94270276fe9a146c472aefe
Anton Stryapunin <anton.stryapunin@gmail.com>
2013-11-05 16:47:05 +0200

    Added image size to response for Google plus service

--- lib/social_feed_agregator/feed.rb
+++ lib/social_feed_agregator/feed.rb
@@ -1,39 +1,42 @@
 module SocialFeedAgregator
   class Feed
 
-    attr_accessor :feed_type,      # :facebook, :twitter or :linkedin
-                  :feed_id,        # origin feed id
-                  :user_id,        # user id
-                  :user_name,      # user name
-                  :permalink,      
-                  :description,    
-                  :picture_url,    
+    attr_accessor :feed_type, # :facebook, :twitter or :linkedin
+                  :feed_id, # origin feed id
+                  :user_id, # user id
+                  :user_name, # user name
+                  :permalink,
+                  :description,
+                  :picture_url,
+                  :picture_height,
+                  :picture_width,
                   :name,
-                  :link, 
+                  :link,
                   # :story, 
-                  :message,          # status message
-                  :caption, 
-                  :created_at, 
-                  :type              # link or status
-    
-    def initialize(options={}) 
+                  :message, # status message
+                  :caption,
+                  :created_at,
+                  :type # link or status
+
+    def initialize(options={})
       @feed_type = options[:feed_type]
-      @feed_id =   options[:feed_id]
+      @feed_id   = options[:feed_id]
 
-      @user_id =   options[:user_id]      
+      @user_id   = options[:user_id]
       @user_name = options[:user_name]
-      
-      @permalink =   options[:permalink]
-      @description = options[:description]
-      @picture_url = options[:picture_url]
 
-      @name  = options[:name]
-      @link  = options[:link]
-      # @story = options[:story]
-      @message = options[:message]
-      @caption = options[:caption]
+      @permalink      = options[:permalink]
+      @description    = options[:description]
+      @picture_url    = options[:picture_url]
+      @picture_height = options[:picture_height]
+      @picture_width  = options[:picture_width]
+
+      @name       = options[:name]
+      @link       = options[:link]
+      @message    = options[:message]
+      @caption    = options[:caption]
       @created_at = options[:created_at]
-      @type = options[:type]              
+      @type       = options[:type]
     end
 
   end
